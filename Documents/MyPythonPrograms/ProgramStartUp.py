73c54fb806594cb0efc659b1fbc3f9d3d8a560ff
pkblanks <pkblanks@yahoo.com>
2017-06-18 13:39:27 +0100

    Added first python program

---
+++ Documents/MyPythonPrograms/ProgramStartUp.py
@@ -0,0 +1,21 @@
+# import subprocess 
+
+# subprocess.call("");
+# subprocess.call("C:\Program Files\Sublime Text 3\sublime_text");
+
+
+# import subprocess 
+# subprocess.call("notepad");
+
+# import subprocess 
+
+#using file path 
+import subprocess 
+subprocess.call("notepad");
+subprocess.call("C:\Program Files\Sublime Text 3\sublime_text");
+subprocess.call("C:\Program Files\iTunes\iTunes.exe");
+# copy and paste python file in C:\Documents and Settings\All Users\Start Menu\Programs\StartUp. this is a hidden destination and will start programs simultaneously on startup.
+
+
+
+
